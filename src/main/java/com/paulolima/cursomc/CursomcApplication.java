18188b082f5b5b10bdff6f7e3500d328236d2c46
paulolimadev <paulonlimadf@gmail.com>
2020-06-09 12:52:36 -0300

    REST testado

--- src/main/java/com/paulolima/cursomc/CursomcApplication.java
+++ src/main/java/com/paulolima/cursomc/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.couse;
+package com.paulolima.cursomc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/paulo/lima/cursomc/CursomcApplicationTests.java
+++ src/test/java/com/paulo/lima/cursomc/CursomcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.educandoweb.couse;
+package com.paulo.lima.cursomc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
