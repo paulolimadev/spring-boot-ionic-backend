e5e099ae93a831bb7212e63edb48f69da2e01ac5
paulolimadev <paulonlimadf@gmail.com>
2020-06-10 15:00:45 -0300

    Classe dominio - Categoria

--- src/main/java/com/paulolima/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/paulolima/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.paulolima.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.paulolima.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando!";
+	public List<Categoria> listar() {
+		//Só para testar...
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
