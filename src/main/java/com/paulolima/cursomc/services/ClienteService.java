55fd9da2a085c80d43e2de12b200066ee5c64fad
paulolimadev <paulonlimadf@gmail.com>
2020-07-08 12:40:07 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/paulolima/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.paulolima.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.paulolima.cursomc.domain.Cliente;
+import com.paulolima.cursomc.repositories.ClienteRepository;
+import com.paulolima.cursomc.services.exception.ObjectNotFoundException;
+
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
