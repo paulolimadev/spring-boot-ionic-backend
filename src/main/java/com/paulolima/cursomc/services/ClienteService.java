99ab26a1bd35204bb7150a3726b78a9c1d0dc9a2
paulolimadev <paulonlimadf@gmail.com>
2020-08-17 12:18:46 -0300

    Validacao custumizada: email não repetido na atualizacao do cliente

--- src/main/java/com/paulolima/cursomc/services/ClienteService.java
+++ src/main/java/com/paulolima/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.paulolima.cursomc.domain.Cidade;
 import com.paulolima.cursomc.domain.Cliente;
@@ -44,7 +45,7 @@ public class ClienteService {
 	}
 
 	
-	//@Transactional
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
 		obj = repo.save(obj);
